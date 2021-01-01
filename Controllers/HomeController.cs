70e79720f352a5356b4d8c2dfbd6298cb415da91
ayoub nemmassi <ayoub.nemmassi@gmail.com>
2021-01-01 22:36:39 +0100

    login 1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookingBus.Models;
+
+namespace BookingBus.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Utilisateur objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (BookingBusEntities  db = new BookingBusEntities ())
+                {
+                    var obj = db.Utilisateurs.Where(a => a.mail.Equals(objUser.mail) && a.mdp.Equals(objUser.mdp)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id_utilisateur.ToString();
+                        Session["UserName"] = obj.nom_complet.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
